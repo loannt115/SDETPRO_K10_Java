e2c085b9489006ec969dd75750f5e7de3eea1f69
Loan Nguyen <muatrenphobayxa115@gmail.com>
2022-09-28 22:46:26 +0700

    Update: check if array list empty AND have more than 1 search number in the array list

--- src/lab05/ArrayListMenu.java
+++ src/lab05/ArrayListMenu.java
@@ -6,6 +6,12 @@ import java.util.Scanner;
 public class ArrayListMenu {
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<Integer>();
+//        arrayList.add(2);
+//        arrayList.add(4);
+//        arrayList.add(6);
+//        arrayList.add(8);
+//        arrayList.add(4);
+//        arrayList.add(4);
         int selectedNumber;
         System.out.println("=====MENU======\n" +
                 "1. Add number into ArrayList: \n" +
@@ -25,21 +31,30 @@ public class ArrayListMenu {
                 arrayList.add(insertedNumber);
                 System.out.println("==> " + insertedNumber + " is added to the array!");
             } else if (selectedNumber == 2) {
-                System.out.println("==> Current array: " + arrayList);
+                if (arrayList.isEmpty()) {
+                    System.out.println("==> The array is empty!");
+                } else
+                    System.out.println("==> Current array: " + arrayList);
             } else if (selectedNumber == 3) {
-                System.out.println("==> Maximum number: " + maximumOfArrayList(arrayList));
+                if (arrayList.isEmpty()) {
+                    System.out.println("==> The array is empty!");
+                } else
+                    System.out.println("==> Maximum number: " + maximumOfArrayList(arrayList));
             } else if (selectedNumber == 4) {
-                System.out.println("==> Minimum number: " + minimumOfArrayList(arrayList));
+                if (arrayList.isEmpty()) {
+                    System.out.println("==> The array is empty!");
+                } else
+                    System.out.println("==> Minimum number: " + minimumOfArrayList(arrayList));
             } else if (selectedNumber == 5) {
                 Scanner scannerSearchNumber = new Scanner(System.in);
                 System.out.print("Input the number you want to search: ");
                 int searchNumber = scannerSearchNumber.nextInt();
-                System.out.println(indexOfNumber(searchNumber, arrayList));
+                indexOfNumberInArray(searchNumber, arrayList);
             } else if (selectedNumber != 0) System.out.println("Please choose correct menu!");
         } while (selectedNumber != 0);
     }
 
-    private static Integer maximumOfArrayList(ArrayList<Integer> arrayList) {
+    private static int maximumOfArrayList(ArrayList<Integer> arrayList) {
         int maxNumber = arrayList.get(0);
         for (int index = 0; index < arrayList.size(); index++) {
             if (arrayList.get(index) > maxNumber) maxNumber = arrayList.get(index);
@@ -57,11 +72,32 @@ public class ArrayListMenu {
         return minNumber;
     }
 
-    private static String indexOfNumber(int searchNumber, ArrayList<Integer> arrayList) {
-        int indexOfSearchNumber = arrayList.indexOf(searchNumber) + 1;
+    private static void indexOfNumberInArray(int searchNumber, ArrayList<Integer> arrayList) {
         if (arrayList.indexOf(searchNumber) == -1) {
-            return ("==> " + searchNumber + " is not in the array.");
-        } else
-            return ("==> " + searchNumber + " ở vị trí thứ " + indexOfSearchNumber + " trong mảng.");
+            System.out.println("==> " + searchNumber + " is not in the array.");
+        } else {
+            int countSearchNumber = 0;
+            ArrayList<Integer> positionOfNumber = new ArrayList<>();
+            String positionOfNumberStr = "";
+            for (int index = 0; index < arrayList.size(); index++) {
+                if (arrayList.get(index) == searchNumber) {
+                    countSearchNumber++;
+                    positionOfNumber.add(index + 1);
+                }
+            }
+            if (countSearchNumber == 1) {
+                positionOfNumberStr = positionOfNumber.get(0).toString();
+            } else if (countSearchNumber == 2) {
+                positionOfNumberStr = positionOfNumber.get(0) + " và " + positionOfNumber.get(1);
+            } else {
+                for (int index = 0; index < (positionOfNumber.size() - 2); index++) {
+                    positionOfNumberStr = positionOfNumberStr + positionOfNumber.get(index) + ", ";
+                }
+                positionOfNumberStr = positionOfNumberStr
+                                        + positionOfNumber.get(positionOfNumber.size() - 2)
+                                        + " và " + + positionOfNumber.get(positionOfNumber.size() - 1);
+            }
+            System.out.println("==> Có " + countSearchNumber + " số " + searchNumber + " trong mảng ở vị trí thứ " + positionOfNumberStr + ".");
+        }
     }
 }
